ecf657214cc5779c3d09ee9dd7e5570ca4676fad
Armon Butler <butlercomarketingagency@gmail.com>
2026-04-02 14:59:57 -0500

    add client and deal modals to dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -9,6 +9,9 @@ import {
   getRevenueStability,
   getForecastScenarios,
   seedDemoData,
+  getClients,
+  createClient,
+  createDeal,
 } from "../api";
 
 import { useNavigate } from "react-router-dom";
