0db179c3db69b4ef3b2d86a67cf4272f4f8b4492
Armon Butler <butlercomarketingagency@gmail.com>
2026-04-05 03:16:03 -0500

    Import apiGet in Integrations page

--- src/pages/Integrations.jsx
+++ src/pages/Integrations.jsx
@@ -7,9 +7,7 @@ import {
   getActiveOrgName,
   getActiveWorkspace,
   uploadSpreadsheetData,
-  getIntegrationAuthUrl,
-  getHubSpotStatus,
-  syncHubSpot,
+  apiGet,
 } from "../api";
 
 const connectorCatalog = [
